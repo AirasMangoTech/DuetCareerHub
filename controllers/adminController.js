ad2ed92b4f49de89057ea00643968ada668e5ef4
wahabqadir761 <wahabqadir761@gmail.com>
2025-05-17 02:46:03 +0500

    Update adminController.js

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -87,6 +87,7 @@ exports.updateAdmin = async (req, res) => {
     if (!updatedAdmin)
       return res.status(404).json({ message: "Admin not found!" });
     res.status(200).json(updatedAdmin);
+    
   } catch (error) {
     res.status(400).json({ message: error.message });
   }
