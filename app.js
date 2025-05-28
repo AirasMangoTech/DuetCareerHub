a099893ee36dde519beef33c1f8cfcd7db4ba1f6
wahabqadir761 <wahabqadir761@gmail.com>
2025-05-29 00:15:27 +0500

    Update app.js

--- app.js
+++ app.js
@@ -41,7 +41,7 @@ app.use(
       "http://localhost:3003",
       "http://localhost:5173",
       "https://duet-hub.vercel.app/",
-      "https://duet-hub.surge.sh/login",
+      "https://duet-hub.surge.sh",
     ],
     methods: "GET,POST,PUT,DELETE",
     credentials: true,
