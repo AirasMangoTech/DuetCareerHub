0eb75f37a9b36caf99c30cf2b910b5f3ebce97da
wahabqadir761 <wahabqadir761@gmail.com>
2025-06-13 00:27:51 +0500

    Create chat.js

---
+++ models/chat.js
@@ -0,0 +1,36 @@
+const mongoose = require("mongoose");
+
+const chatSchema = new mongoose.Schema({
+  sender: {
+    type: mongoose.Schema.Types.ObjectId,
+    refPath: "senderRole",
+  },
+  senderRole: {
+    type: String,
+    enum: ["Faculty", "Alumni", "User"],
+  },
+  receiver: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    refPath: "receiverRole",
+  },
+  receiverRole: {
+    type: String,
+    enum: ["Faculty", "Alumni", "User"],
+  },
+  message: {
+    type: String,
+    required: true,
+  },
+
+  seen: {
+    type: Boolean,
+    default: false,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+module.exports = mongoose.model("Chat", chatSchema);
